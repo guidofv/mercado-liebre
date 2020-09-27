3e92a488df91970bd55c3e1c62d3c3004ecb3cab
guidofv <guidovitale@gmail.com>
2020-09-27 17:13:44 -0300

    Añadida la primera parte del ejercicio de Organizando Elementos

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ app.listen(3030,()=>{
 app.get('/',function(req,res){
     let file = path.resolve('index.html');
     res.sendFile(file);
-})
+});
+
+app.get('/register',function(req,res){
+    let file = path.resolve('register.html');
+    res.sendFile(file);
+});
+
+app.get('/login',function(req,res){
+    let file = path.resolve('login.html');
+    res.sendFile(file);
+});
 
 app.get('*',function(req,res){
     if(req.url.endsWith('.css')){
